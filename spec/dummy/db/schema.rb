82525ef56dbb2032de2e392ff2bde14812b994c2
bradrobertson <brad@influitive.com>
2011-12-05 18:00:54 -0500

    fixing schema, need a better way of ensuring this file doesnt get changed/committed

--- spec/dummy/db/schema.rb
+++ spec/dummy/db/schema.rb
@@ -11,6 +11,32 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 0) do
+ActiveRecord::Schema.define(:version => 20110613152810) do
+
+  create_table "companies", :force => true do |t|
+    t.boolean "dummy"
+    t.string  "database"
+  end
+
+  create_table "delayed_jobs", :force => true do |t|
+    t.integer  "priority",   :default => 0
+    t.integer  "attempts",   :default => 0
+    t.text     "handler"
+    t.text     "last_error"
+    t.datetime "run_at"
+    t.datetime "locked_at"
+    t.datetime "failed_at"
+    t.string   "locked_by"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "delayed_jobs", ["priority", "run_at"], :name => "delayed_jobs_priority"
+
+  create_table "users", :force => true do |t|
+    t.string   "name"
+    t.datetime "birthdate"
+    t.string   "sex"
+  end
 
 end
