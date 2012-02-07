b5a8e6ebe43063cd3f8b6e08489f797aba957d93
bradrobertson <brad@influitive.com>
2012-02-07 16:34:54 -0500

    working on stripping out rails dependency, hopefully get it down to just activerecord soon

--- apartment.gemspec
+++ apartment.gemspec
@@ -12,20 +12,22 @@ Gem::Specification.new do |s|
   s.email = %w{ryan@ryanbrunner.com bradleyrobertson@gmail.com}
   s.files         = `git ls-files`.split("\n")
   s.test_files    = `git ls-files -- {spec}/*`.split("\n")
-  
+
   s.homepage = %q{http://github.com/bradrobertson/apartment}
   s.licenses = ["MIT"]
   s.require_paths = ["lib"]
   s.rubygems_version = %q{1.3.7}
-  
-  s.add_dependency 'rails', '~> 3.0.10'   # must be >= 3.0.10 due to poor schema support pre 3.0.10, but < 3.1 because it hasn't been fully tested yet
-  s.add_development_dependency 'rake', '~> 0.8.7'
+
+  s.add_dependency 'activerecord',    '~> 3.0.10'   # must be >= 3.0.10 due to poor schema support pre 3.0.10
+  s.add_dependency 'actionpack',  '~> 3.0.10'   # must be >= 3.0.10 due to poor schema support pre 3.0.10
+
+  s.add_development_dependency 'rake',            '~> 0.8.7'
   s.add_development_dependency 'sqlite3'
-  s.add_development_dependency 'rspec', '~> 2.6.0'
-  s.add_development_dependency 'rspec-rails', '~> 2.6.1'
-  s.add_development_dependency 'capybara', '1.0.0'
-  s.add_development_dependency 'pg', '~> 0.11.0'
-  s.add_development_dependency 'mysql2', '0.2.7'
+  s.add_development_dependency 'rspec',           '~> 2.6.0'
+  s.add_development_dependency 'rspec-rails',     '~> 2.6.1'
+  s.add_development_dependency 'capybara',        '1.0.0'
+  s.add_development_dependency 'pg',              '~> 0.11.0'
+  s.add_development_dependency 'mysql2',          '0.2.7'
   s.add_development_dependency "silent-postgres", "~> 0.1.1"
-  s.add_development_dependency 'delayed_job', '~> 2.1.4'
+  s.add_development_dependency 'delayed_job',     '~> 2.1.4'
 end
--- lib/apartment.rb
+++ lib/apartment.rb
@@ -1,16 +1,16 @@
 require 'apartment/railtie' if defined?(Rails)
 
 module Apartment
-  
+
   class << self
     attr_accessor :use_postgres_schemas, :seed_after_create, :prepend_environment
     attr_writer :database_names, :excluded_models
-    
+
     # configure apartment with available options
     def configure
       yield self if block_given?
     end
-    
+
     # Be careful not to use `return` here so both Proc and lambda can be used without breaking
     def database_names
       if @database_names.respond_to?(:call)
@@ -19,55 +19,55 @@ module Apartment
         @database_names
       end
     end
-    
+
     # Default to none
     def excluded_models
       @excluded_models || []
     end
-    
+
   end
-  
+
   autoload :Database, 'apartment/database'
   autoload :Migrator, 'apartment/migrator'
   autoload :Reloader, 'apartment/reloader'
-  
+
   module Adapters
     autoload :AbstractAdapter, 'apartment/adapters/abstract_adapter'
     # Specific adapters will be loaded dynamically based on adapter in config
   end
-  
+
   module Elevators
     autoload :Subdomain, 'apartment/elevators/subdomain'
   end
-  
+
   module Delayed
-    
+
     autoload :Requirements, 'apartment/delayed_job/requirements'
-    
+
     module Job
       autoload :Hooks, 'apartment/delayed_job/hooks'
     end
   end
-  
+
   # Exceptions
   class ApartmentError < StandardError; end
-  
+
   # Raised when apartment cannot find the adapter specified in <tt>config/database.yml</tt>
   class AdapterNotFound < ApartmentError; end
-  
+
   # Raised when database cannot find the specified database
   class DatabaseNotFound < ApartmentError; end
-  
+
   # Raised when trying to create a database that already exists
   class DatabaseExists < ApartmentError; end
-  
+
   # Raised when database cannot find the specified schema
   class SchemaNotFound < ApartmentError; end
-  
+
   # Raised when trying to create a schema that already exists
   class SchemaExists < ApartmentError; end
-  
+
   # Raised when an ActiveRecord object does not have the required database field on it
   class DJSerializationError < ApartmentError; end
-  
+
 end
