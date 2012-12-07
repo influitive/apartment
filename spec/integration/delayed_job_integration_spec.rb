350aef522e577968d29576c066aeb1455f3a8fa4
Joshua Schniper <Joshua.Schniper@gmail.com>
2012-12-06 22:19:58 -0600

    - Added JRuby to Travis CI yml file - Ignoring the DelayedJob tests in JRuby so all tests pass

--- spec/integration/delayed_job_integration_spec.rb
+++ spec/integration/delayed_job_integration_spec.rb
@@ -3,85 +3,87 @@ require 'delayed_job'
 require 'delayed_job_active_record'
 
 describe Apartment::Delayed do
+  unless defined?(JRUBY_VERSION)
 
-  # See apartment.yml file in dummy app config
+    # See apartment.yml file in dummy app config
 
-  let(:config){ Apartment::Test.config['connections']['postgresql'].symbolize_keys }
-  let(:database){ Apartment::Test.next_db }
-  let(:database2){ Apartment::Test.next_db }
-
-  before do
-    ActiveRecord::Base.establish_connection config
-    Apartment::Test.load_schema   # load the Rails schema in the public db schema
-    Apartment::Database.stub(:config).and_return config   # Use postgresql database config for this test
-
-    Apartment.configure do |config|
-      config.use_postgres_schemas = true
-    end
-
-    Apartment::Database.create database
-    Apartment::Database.create database2
-  end
-
-  after do
-    Apartment::Test.drop_schema database
-    Apartment::Test.drop_schema database2
-    Apartment.reset
-  end
-
-  describe Apartment::Delayed::Requirements do
+    let(:config) { Apartment::Test.config['connections']['postgresql'].symbolize_keys }
+    let(:database) { Apartment::Test.next_db }
+    let(:database2) { Apartment::Test.next_db }
 
     before do
-      Apartment::Database.switch database
-      User.send(:include, Apartment::Delayed::Requirements)
-      User.create
-    end
+      ActiveRecord::Base.establish_connection config
+      Apartment::Test.load_schema # load the Rails schema in the public db schema
+      Apartment::Database.stub(:config).and_return config # Use postgresql database config for this test
 
-    it "should initialize a database attribute on a class" do
-      user = User.first
-      user.database.should == database
-    end
-
-    it "should not overwrite any previous after_initialize declarations" do
-      User.class_eval do
-        after_find :set_name
-
-        def set_name
-          self.name = "Some Name"
-        end
+      Apartment.configure do |config|
+        config.use_postgres_schemas = true
       end
 
-      user = User.first
-      user.database.should == database
-      user.name.should == "Some Name"
+      Apartment::Database.create database
+      Apartment::Database.create database2
     end
 
-    it "should set the db on a new record before it saves" do
-      user = User.create
-      user.database.should == database
+    after do
+      Apartment::Test.drop_schema database
+      Apartment::Test.drop_schema database2
+      Apartment.reset
     end
 
-    context "serialization" do
-      it "should serialize the proper database attribute" do
-        user_yaml = User.first.to_yaml
-        Apartment::Database.switch database2
-        user = YAML.load user_yaml
+    describe Apartment::Delayed::Requirements do
+
+      before do
+        Apartment::Database.switch database
+        User.send(:include, Apartment::Delayed::Requirements)
+        User.create
+      end
+
+      it "should initialize a database attribute on a class" do
+        user = User.first
         user.database.should == database
       end
+
+      it "should not overwrite any previous after_initialize declarations" do
+        User.class_eval do
+          after_find :set_name
+
+          def set_name
+            self.name = "Some Name"
+          end
+        end
+
+        user = User.first
+        user.database.should == database
+        user.name.should == "Some Name"
+      end
+
+      it "should set the db on a new record before it saves" do
+        user = User.create
+        user.database.should == database
+      end
+
+      context "serialization" do
+        it "should serialize the proper database attribute" do
+          user_yaml = User.first.to_yaml
+          Apartment::Database.switch database2
+          user = YAML.load user_yaml
+          user.database.should == database
+        end
+      end
     end
-  end
 
-  describe Apartment::Delayed::Job::Hooks do
+    describe Apartment::Delayed::Job::Hooks do
 
-    let(:worker){ Delayed::Worker.new }
-    let(:job){ Delayed::Job.enqueue User.new }
+      let(:worker) { Delayed::Worker.new }
+      let(:job) { Delayed::Job.enqueue User.new }
 
-    it "should switch to previous db" do
-      Apartment::Database.switch database
-      worker.run(job)
+      it "should switch to previous db" do
+        Apartment::Database.switch database
+        worker.run(job)
 
-      Apartment::Database.current_database.should == database
+        Apartment::Database.current_database.should == database
+      end
     end
-  end
 
+  end
 end
