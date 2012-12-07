c8842b546cfa50d52793bf3574d2f4575b78bab6
bradrobertson <brad@influitive.com>
2012-12-07 18:03:44 -0500

    whooops, backwards schema/vs non-schema implementation

--- lib/apartment/adapters/mysql2_adapter.rb
+++ lib/apartment/adapters/mysql2_adapter.rb
@@ -4,33 +4,14 @@ module Apartment
 
     def self.mysql2_adapter(config)
       Apartment.use_schemas ?
-        Adapters::Mysql2Adapter.new(config) :
-        Adapters::Mysql2SchemaAdapter.new(config)
+        Adapters::Mysql2SchemaAdapter.new(config) :
+        Adapters::Mysql2Adapter.new(config)
     end
   end
 
   module Adapters
 
     class Mysql2Adapter < AbstractAdapter
-
-    protected
-      #   Set schema search path to new schema
-      #
-      def connect_to_new(database = nil)
-        return reset if database.nil?
-
-        @current_database = database.to_s
-        Apartment.connection.execute "use #{@current_database}"
-
-
-      rescue ActiveRecord::StatementInvalid
-        Apartment::Database.reset
-        raise DatabaseNotFound, "Cannot find database #{environmentify(database)}"
-      end
-
-    end
-
-    class Mysql2SchemaAdapter < AbstractAdapter
 
     protected
 
@@ -40,9 +21,26 @@ module Apartment
       #
       #   @param {String} database Database name
       #
-      def connect_to_new(database)
+      def connect_to_new(database = nil)
         super
       rescue Mysql2::Error
+        Apartment::Database.reset
+        raise DatabaseNotFound, "Cannot find database #{environmentify(database)}"
+      end
+    end
+
+    class Mysql2SchemaAdapter < AbstractAdapter
+
+    protected
+
+      #   Set schema search path to new schema
+      #
+      def connect_to_new(database)
+        return reset if database.nil?
+
+        Apartment.connection.execute "use #{database}"
+
+      rescue ActiveRecord::StatementInvalid
         Apartment::Database.reset
         raise DatabaseNotFound, "Cannot find database #{environmentify(database)}"
       end
