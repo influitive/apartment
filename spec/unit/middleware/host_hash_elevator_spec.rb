f74425f773b72ffd0fbad811c6c23feded383d1a
Guillaume DOTT <guillaume.dott@lafourmi-immo.com>
2013-05-14 15:24:57 +0200

    add HostHash elevator
    allows to switch database based on full host and using
    a hash to find corresponding database name

---
+++ spec/unit/middleware/host_hash_elevator_spec.rb
@@ -0,0 +1,26 @@
+require 'spec_helper'
+
+describe Apartment::Elevators::HostHash do
+
+  describe "#parse_database_name" do
+    it "parses the host for a domain name" do
+      request = ActionDispatch::Request.new('HTTP_HOST' => 'example.com')
+      elevator = Apartment::Elevators::HostHash.new(nil, 'example.com' => 'example_database')
+      elevator.parse_database_name(request).should == 'example_database'
+    end
+
+    it "raises DatabaseNotFound exception if there is no host" do
+      request = ActionDispatch::Request.new('HTTP_HOST' => '')
+      elevator = Apartment::Elevators::HostHash.new(nil, 'example.com' => 'example_database')
+      expect { elevator.parse_database_name(request) }.to raise_error(Apartment::DatabaseNotFound)
+    end
+
+    it "raises DatabaseNotFound exception if there is no database associated to current host" do
+      request = ActionDispatch::Request.new('HTTP_HOST' => 'example2.com')
+      elevator = Apartment::Elevators::HostHash.new(nil, 'example.com' => 'example_database')
+      expect { elevator.parse_database_name(request) }.to raise_error(Apartment::DatabaseNotFound)
+    end
+
+  end
+
+end
