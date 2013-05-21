1705112953235d4c5e577e0a3cd508d53528c49a
Brad Robertson <brad@influitive.com>
2013-05-21 08:55:30 -0400

    simplify first subdomain impl

--- lib/apartment/elevators/first_subdomain.rb
+++ lib/apartment/elevators/first_subdomain.rb
@@ -1,14 +1,15 @@
 module Apartment
   module Elevators
-    # Provides a rack based db switching solution based on subdomains
-    # Assumes that database name should match subdomain
+    # Provides a rack based db switching solution based on the first subdomain
+    # of a given domain name.
+    # eg:
+    #     - example1.domain.com               => example1
+    #     - example2.something.domain.com     => example2
     class FirstSubdomain < Subdomain
 
       def parse_database_name(request)
-        subdomain = super(request)
-        subdomain && subdomain.match(/(\w+)(\.\w+)?/)[1]
+        super.split('.')[0]
       end
-
     end
   end
 end
