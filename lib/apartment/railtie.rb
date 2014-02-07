ba9d1e0cfa060a3c98e21b0606c29db03a9e7d1f
Rafał Trojanowski <rafal@prograils.com>
2014-02-07 09:56:31 +0100

    make tld_length configurable in Apartment

--- lib/apartment/railtie.rb
+++ lib/apartment/railtie.rb
@@ -17,6 +17,7 @@ module Apartment
         config.seed_after_create = false
         config.prepend_environment = false
         config.append_environment = false
+        config.tld_length = 1
       end
 
       ActiveRecord::Migrator.migrations_paths = Rails.application.paths['db/migrate'].to_a
