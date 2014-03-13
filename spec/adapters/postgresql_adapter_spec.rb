f8bc833c9f7d04767b676df69f9fa2717546a31e
Veljko Dragsic <veljko@kset.org>
2014-03-13 12:58:19 +0100

    Postgresql schema adapter that uses raw SQL dumps.

--- spec/adapters/postgresql_adapter_spec.rb
+++ spec/adapters/postgresql_adapter_spec.rb
@@ -6,9 +6,24 @@ describe Apartment::Adapters::PostgresqlAdapter, database: :postgresql do
 
     subject{ Apartment::Database.postgresql_adapter config }
 
-    context "using schemas" do
+    context "using schemas with schema.rb" do
 
       before{ Apartment.use_schemas = true }
+
+      # Not sure why, but somehow using let(:tenant_names) memoizes for the whole example group, not just each test
+      def tenant_names
+        ActiveRecord::Base.connection.execute("SELECT nspname FROM pg_namespace;").collect { |row| row['nspname'] }
+      end
+
+      let(:default_tenant) { subject.process { ActiveRecord::Base.connection.schema_search_path.gsub('"', '') } }
+
+      it_should_behave_like "a generic apartment adapter"
+      it_should_behave_like "a schema based apartment adapter"
+    end
+
+    context "using schemas with SQL dump" do
+
+      before{ Apartment.use_schemas = true; Apartment.use_sql = true }
 
       # Not sure why, but somehow using let(:tenant_names) memoizes for the whole example group, not just each test
       def tenant_names
