a5310cf9dc1ff89185a6dfa1b17de82db9afb2cd
Zulfiqar Ali <desheikh@gmail.com>
2015-01-06 22:32:33 -0500

    improve initializer documentation

--- lib/generators/apartment/install/templates/apartment.rb
+++ lib/generators/apartment/install/templates/apartment.rb
@@ -1,4 +1,6 @@
-# Require whichever elevator you're using below here...
+# You can have Apartment route to the appropriate Tenant by adding some Rack middleware.
+# Apartment can support many different "Elevators" that can take care of this routing to your data.
+# Require whichever Elevator you're using below or none if you have a custom one.
 #
 # require 'apartment/elevators/generic'
 # require 'apartment/elevators/domain'
@@ -9,43 +11,57 @@ require 'apartment/elevators/subdomain'
 #
 Apartment.configure do |config|
 
-  # These models will not be multi-tenanted,
-  # but remain in the global (public) namespace
+  # Add any models that you do not want to be multi-tenanted, but remain in the global (public) namespace.
+  # A typical example would be a Customer or Tenant model that stores each Tenant's information.
   #
-  # An example might be a Customer or Tenant model that stores each tenant information
-  # ex:
+  # config.excluded_models = %w{ Tenant }
+
+  # In order to migrate all of your Tenants you need to provide a list of Tenant names to Apartment.
+  # You can make this dynamic by providing a Proc object to be called on migrations.
+  # This object should yield an array of strings representing each Tenant name.
   #
-  # config.excluded_models = %w{Tenant}
+  # config.tenant_names = lambda{ Customer.pluck(:tenant_name) }
+  # config.tenant_names = ['tenant1', 'tenant2']
   #
-  config.excluded_models = %w{}
+  config.tenant_names = lambda { ToDo_Tenant_Or_User_Model.pluck :database }
 
-  # use postgres schemas?
-  config.use_schemas = true
+  #
+  # ==> PostgreSQL only options
 
-  # use raw SQL dumps for creating postgres schemas? (only applies with use_schemas set to true)
-  #config.use_sql = true
+  # Specifies whether to use PostgreSQL schemas or create a new database per Tenant.
+  # The default behaviour is true.
+  #
+  # config.use_schemas = true
 
-  # configure persistent schemas (E.g. hstore )
+  # Apartment can be forced to use raw SQL dumps insted of schema.rb for creating new schemas.
+  # Use this when you are using some extra features in PostgreSQL that can't be respresented in
+  # schema.rb, like materialized views etc. (only applies with use_schemas set to true).
+  # (Note: this option doesn't use db/structure.sql, it creates SQL dump by executing pg_dump)
+  #
+  # config.use_sql = false
+
+  # There are cases where you might want some schemas to always be in your search_path
+  # e.g when using a PostgreSQL extension like hstore.
+  # Any schemas added here will be available along with your selected Tenant.
+  #
   # config.persistent_schemas = %w{ hstore }
 
-  # add the Rails environment to database names?
-  # config.prepend_environment = true
-  # config.append_environment = true
+  # <== PostgreSQL only options
+  #
 
-  # supply list of database names for migrations to run on
-  config.tenant_names = lambda{ ToDo_Tenant_Or_User_Model.pluck :database }
-
-  # Specify a connection other than ActiveRecord::Base for apartment to use (only needed if your models are using a different connection)
-  # config.connection_class = ActiveRecord::Base
+  # By default, and only when not using PostgreSQL schemas, Apartment will prepend the environment
+  # to the tenant name to ensure there is no conflict between your environments.
+  # This is mainly for the benefit of your development and test environments.
+  # Uncomment the line below if you want to disable this behaviour in production.
+  #
+  # config.prepend_environment = !Rails.env.production?
 end
 
-##
-# Elevator Configuration
-
+# Setup a custom Tenant switching middleware. The Proc should return the name of the Tenant that
+# you want to switch to.
 # Rails.application.config.middleware.use 'Apartment::Elevators::Generic', lambda { |request|
-#   # TODO: supply generic implementation
+#   request.host.split('.').first
 # }
 
 # Rails.application.config.middleware.use 'Apartment::Elevators::Domain'
-
 Rails.application.config.middleware.use 'Apartment::Elevators::Subdomain'
