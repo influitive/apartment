30e096cdcfff1bc189e0dab36beb3de401303254
Andy Liu <lcwind9711@gmail.com>
2019-03-25 11:08:44 +0800

    add sqlserver adapter

--- apartment.gemspec
+++ apartment.gemspec
@@ -41,5 +41,7 @@ Gem::Specification.new do |s|
     s.add_development_dependency 'mysql2'
     s.add_development_dependency 'pg'
     s.add_development_dependency 'sqlite3'
+    s.add_development_dependency 'tiny_tds'
+    s.add_development_dependency 'activerecord-sqlserver-adapter'
   end
 end
---
+++ lib/apartment/adapters/sqlserver_adapter.rb
@@ -0,0 +1,22 @@
+require 'apartment/adapters/abstract_adapter'
+
+module Apartment
+   module Tenant
+
+     def self.sqlserver_adapter(config)
+      config['default_schema'] = 'dbo' if config['default_schema'].eql?('public')
+      Adapters::SqlserverAdapter.new config
+    end
+  end
+
+   module Adapters
+
+     class SqlserverAdapter < AbstractAdapter
+
+     protected
+       def rescue_from
+         TinyTds::Error
+       end
+    end
+  end
+end
