6c7d1fecd7149acbb7f43ba72d9228d12f7df960
Rui Baltazar <rui.p.baltazar@gmail.com>
2020-02-16 17:26:49 +0800

    WIP - fixed yaml safe load

--- spec/support/config.rb
+++ spec/support/config.rb
@@ -5,7 +5,9 @@ require 'yaml'
 module Apartment
   module Test
     def self.config
+      # rubocop:disable Security/YAMLLoad
       @config ||= YAML.load(ERB.new(IO.read('spec/config/database.yml')).result)
+      # rubocop:enable Security/YAMLLoad
     end
   end
 end
