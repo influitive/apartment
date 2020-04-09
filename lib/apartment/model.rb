d141639f3b6a809e1911fd956519f589d1124e5c
Rui Baltazar <rui.p.baltazar@gmail.com>
2020-04-09 09:45:51 +0800

    Fixed cache key format. Added notes on cached find_by_statement

--- lib/apartment/model.rb
+++ lib/apartment/model.rb
@@ -5,8 +5,14 @@ module Apartment
     extend ActiveSupport::Concern
 
     module ClassMethods
+      # NOTE: key is actually an array of keys. E.g. If we run the following
+      # query: `Setting.find_by(key: 'something', value: 'amazing')` key will
+      # have an array of symbols: `[:key, :something]`
       def cached_find_by_statement(key, &block)
-        cache_key = "#{Apartment::Tenant.current}_#{key}".to_sym
+        # Modifying the cache key to have a reference to the current tenant,
+        # so the cached statement is referring only to the tenant in which we've
+        # executed this
+        cache_key = [Apartment::Tenant.current] + key
         cache = @find_by_statement_cache[connection.prepared_statements]
         cache.compute_if_absent(cache_key) { ActiveRecord::StatementCache.create(connection, &block) }
       end
