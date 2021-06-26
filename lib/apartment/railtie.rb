3fdd2989115e65de5c3243891d8a5c7c96359418
Rui Baltazar <rui.p.baltazar@gmail.com>
2021-06-26 10:57:53 +0800

    addressed Performance/RedundantEqualityComparisonBlock

--- lib/apartment/railtie.rb
+++ lib/apartment/railtie.rb
@@ -32,7 +32,7 @@ module Apartment
     #
     config.to_prepare do
       next if ARGV.any? { |arg| arg =~ /\Aassets:(?:precompile|clean)\z/ }
-      next if ARGV.any? { |arg| arg == 'webpacker:compile' }
+      next if ARGV.any?('webpacker:compile')
       next if ENV['APARTMENT_DISABLE_INIT']
 
       begin
