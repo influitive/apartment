226d112d8da191b7fc1cc7c0721a5d07f81f4694
Rui Baltazar <rui.p.baltazar@gmail.com>
2022-02-02 12:18:04 +0100

    added some tests

--- lib/apartment/active_record/postgresql_adapter.rb
+++ lib/apartment/active_record/postgresql_adapter.rb
@@ -1,10 +1,15 @@
 # frozen_string_literal: true
 
+# rubocop:disable Style/ClassAndModuleChildren
+
+# NOTE: This patch is meant to remove any schema_prefix appart from the ones for
+# excluded models. The schema_prefix would be resolved by apartment's setting
+# of search path
 module Apartment::PostgreSqlAdapterPatch
   def default_sequence_name(table, _column)
     res = super
     schema_prefix = "#{Apartment::Tenant.current}."
-    if res&.starts_with?(schema_prefix) && Apartment.excluded_models.none?{|m| m.constantize.table_name == table}
+    if res&.starts_with?(schema_prefix) && Apartment.excluded_models.none? { |m| m.constantize.table_name == table }
       res.delete_prefix!(schema_prefix)
     end
     res
@@ -13,6 +18,8 @@ end
 
 require 'active_record/connection_adapters/postgresql_adapter'
 
+# NOTE: inject this into postgresql adapters
 class ActiveRecord::ConnectionAdapters::PostgreSQLAdapter
   include Apartment::PostgreSqlAdapterPatch
 end
+# rubocop:enable Style/ClassAndModuleChildren
