b67bc7a4da864c0522a8d5befec9f252197a7438
Rui Baltazar <rui.p.baltazar@gmail.com>
2022-02-03 01:11:42 +0100

    only require apartment PostgreSQLAdapter if were using postgres

--- lib/apartment.rb
+++ lib/apartment.rb
@@ -8,7 +8,10 @@ require 'apartment/tenant'
 
 require_relative 'apartment/log_subscriber'
 
-require_relative 'apartment/active_record/postgresql_adapter'
+if defined?(ActiveRecord::ConnectionAdapters::PostgreSQLAdapter)
+  require_relative 'apartment/active_record/postgresql_adapter'
+end
+
 if ActiveRecord.version.release >= Gem::Version.new('6.0')
   require_relative 'apartment/active_record/connection_handling'
 end
